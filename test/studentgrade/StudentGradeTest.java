00a556be874f4b44d31ebbc9eecf37648714a662
unknown <wafa2002za@gmail.com>
2024-04-18 20:42:14 +0300

    junit 4

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,35 +1,45 @@
 package studentgrade;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
+/* JUnit 4 */
+public class StudentGradeTest {
     
-    public StudentGradeTest(String testName) {
-        super(testName);
+    public StudentGradeTest() {
+    }
+    
+    @Before
+    public void beforeTest(){
+        System.out.println("Attempt to test");
     }
 
+    @Test
     public void testGetGrade95() {
         System.out.println("getGrade 95");
         int mark = 95;
         String expResult = "A";
         String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+        Assert.assertEquals(expResult, result);
     }
 
+    @Test
     public void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
         String expResult = "B";
         String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+        Assert.assertEquals(expResult, result);
     }
     
+    @Test
     public void testGetGrade75() {
         System.out.println("getGrade 85");
         int mark = 75;
         String expResult = "F";
         String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+        Assert.assertEquals(expResult, result);
     }
 }
